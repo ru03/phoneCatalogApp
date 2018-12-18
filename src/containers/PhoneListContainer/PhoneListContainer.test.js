2c9b6df073c6ec47083363fab2405043c15541e6
ru03 <ruben.lmangas@gmail.com>
2018-12-18 21:23:07 +0100

    add test display spinner in container

--- src/containers/PhoneListContainer/PhoneListContainer.test.js
+++ src/containers/PhoneListContainer/PhoneListContainer.test.js
@@ -3,13 +3,15 @@ import { mount } from 'enzyme';
 import { PhoneListContainer } from './PhoneListContainer';
 import PhoneInfoCard from './../../components/PhoneInfoCard/PhoneInfoCard';
 import PhoneDetails from './../../components/PhoneDetailComponent/PhoneDetailComponent';
+import Spinner from './../../components/UI/Spinner/Spinner';
 
 describe('PhoneListContainer test', () => {
     let phoneListContainer;
     beforeEach(() => {
         const props = {
             phones: [],
-            phone: []
+            phone: [],
+            loading: false
         }
         phoneListContainer = mount(<PhoneListContainer onFetchPhones={() => {}} {...props}/>);
     });
@@ -20,6 +22,13 @@ describe('PhoneListContainer test', () => {
 
     it('PhoneListContainer component should display no items message without phones', () => {
         expect(phoneListContainer.find('.Items').text()).toEqual('No items were found');
+    });
+
+    it('PhoneListContainer component should display spinner if loading is true', () => {
+        phoneListContainer.setProps({
+            loading: true
+        });
+        expect(phoneListContainer.find(Spinner)).toHaveLength(1);
     });
 
     it('PhoneListContainer component should display PhoneInfoCard component', () => {
